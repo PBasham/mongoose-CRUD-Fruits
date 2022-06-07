9bd27565270d3327686958fd5a046dd51a97a6c2
patrickbasham <PatrickTBasham@gmail.com>
2022-06-07 11:57:22 -0400

    created partials layout page, created index route

--- server.js
+++ server.js
@@ -67,7 +67,34 @@ app.use(express.static("public")); // serve files from public statically
 app.get("/", (req, res) => {
     res.send("your server is running... better catch it.");
   });
-  
+
+// NOTE: This route (Seed route) is used to empty your database and set it back to it's starting values. (FOR TESTING)
+app.get("/fruits/seed", (req, res) => {
+    // array of starter fruits
+    const startFruits = [
+        { name: "Orange", color: "orange", readyToEat: false },
+        { name: "Grape", color: "purple", readyToEat: false },
+        { name: "Banana", color: "orange", readyToEat: false },
+        { name: "Strawberry", color: "red", readyToEat: false },
+        { name: "Coconut", color: "brown", readyToEat: false },
+    ];
+
+    // Delete all fruits
+    Fruit.deleteMany({}).then((data) => {
+        // Seed Starter Fruits
+        Fruit.create(startFruits).then((data) => {
+            // send created fruits as response to confirm creation
+            res.json(data);
+        });
+    });
+});
+
+// index route
+app.get("/fruits", async (req, res) => {
+    const fruits = await Fruit.find({})
+    res.render("index", {fruits})
+})
+
 //////////////////////////////////////////////
 // Server Listener
 //////////////////////////////////////////////
